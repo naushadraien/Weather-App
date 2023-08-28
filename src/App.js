011117f69a4ca02f46c89bdfa1f3b2b9edd00217
naushadraien <naushadraien5@gmail.com>
2023-08-28 14:41:32 +0545

    added some logics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { useState } from 'react';
-import './App.css';
-import CurrentWeather from './components/currentWeather/currentWeather';
-import Search from './components/search/search';
-import { WEATHER_API_URL, WEATHER_API_KEY } from './components/api';
-import WeatherForecast from './components/forecasts/weatherForecast';
-import axios from 'axios';
-
+import { useState } from "react";
+import "./App.css";
+import CurrentWeather from "./components/currentWeather/currentWeather";
+import Search from "./components/search/search";
+import WeatherForecast from "./components/forecasts/weatherForecast";
+import axios from "axios";
 
 //Using with axios request
 
@@ -13,53 +11,59 @@ function App() {
   //initial value is null
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
-  const handleOnSearchChange=(searchData)=>{
+  const handleOnSearchChange = (searchData) => {
     //[lat, lon] are variables and searchData.value.split(" ") will split latitude and longitude by space
-   const [lat, lon] = searchData.value.split(" ");
+    const [lat, lon] = searchData.value.split(" ");
 
-   //fetching the current weather
-   //here &units=metric is used for showing the temperature in celcius
-  //  const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
-  
-  //with axios request
-  const currentWeatherFetch = axios.get(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+    const WEATHER_API_KEY = "1c6565ada823323167521d05acd4ed7b";
 
-   //fetching the weather forecast
-   //here &units=metric is used for showing the temperature in celcius
-  //  const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    //fetching the current weather
+    //here &units=metric is used for showing the temperature in celcius
+    //  const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
-  //with axios request
-  const forecastFetch = axios.get(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    //with axios request
+    const currentWeatherFetch = axios.get(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    //fetching the weather forecast
+    //here &units=metric is used for showing the temperature in celcius
+    //  const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+
+    //with axios request
+    const forecastFetch = axios.get(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
 
     Promise.all([currentWeatherFetch, forecastFetch])
-    .then(async(response)=>{
-      //calling the json from api of index 0 for weather and 1 for forecast
-      
-      // const WeatherResponse =  await response[0].json();
-      // const ForecastResponse =  await response[1].json();
+      .then(async (response) => {
+        //calling the json from api of index 0 for weather and 1 for forecast
 
-      //with axios
-      const WeatherResponse =  response[0].data;
-      const ForecastResponse =  response[1].data;
+        // const WeatherResponse =  await response[0].json();
+        // const ForecastResponse =  await response[1].json();
 
+        //with axios
+        const WeatherResponse = response[0].data;
+        const ForecastResponse = response[1].data;
 
-      // these two set get the data of weather and response
-      //displaying the city and country code
-      // here label is coming from the label of search.js
-      //... are spread operator to create new object from WeatherResponse and ForecastResponse
-      setCurrentWeather({ city: searchData.label, ...WeatherResponse});
-      setForecast({ city: searchData.label, ...ForecastResponse});
-    })
-    .catch((err)=> console.log(err));
-  }
+        // these two set get the data of weather and response
+        //displaying the city and country code
+        // here label is coming from the label of search.js
+        //... are spread operator to create new object from WeatherResponse and ForecastResponse
+        setCurrentWeather({ city: searchData.label, ...WeatherResponse });
+        setForecast({ city: searchData.label, ...ForecastResponse });
+      })
+      .catch((err) => console.log(err));
+  };
   // console.log(currentWeather);
   // console.log(forecast);
   return (
     <div className="container">
-     <Search onSearchChange={handleOnSearchChange}/>
-     {/* Here && for checking if the data exist or not. if it exists then show the data and if not so don't show the data */}
-     {currentWeather && <CurrentWeather data={currentWeather}/>}
-     {forecast && <WeatherForecast data={forecast}/>}
+      <Search onSearchChange={handleOnSearchChange} />
+      {/* Here && for checking if the data exist or not. if it exists then show the data and if not so don't show the data */}
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <WeatherForecast data={forecast} />}
     </div>
   );
 }
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,41 +1,58 @@
-import { useState } from 'react';
-import { AsyncPaginate } from 'react-select-async-paginate';
-import { geoAPIUrl, geoAPIOptions } from '../api';
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
 
 //Here search.js is only for searching the cities for latitude, londitude and country code and this is all the code for search,js file
 
 //here onSearchChange function is passed for searching the data
 const Search = ({ onSearchChange }) => {
-    const [search, setSearch] = useState(null);
-    const loadOptions = (inputValue) => {
-        //here after return we cut the code from fetch to .catch function from api.js
-        //here ` ` is used for making the variable in js and ${} are variables inside that curly braces
-        return fetch(`${geoAPIUrl}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoAPIOptions)
-            .then(response => response.json())
-            .then(response => {
-                return{
-                    options: response.data.map((city)=>{
-                        return {
-                            //this is the format for searching the cities for avoiding the loadOptions error in consle
-                            value: `${city.latitude} ${city.longitude}`, 
-                            label: `${city.name}, ${city.countryCode}`,
-                        }
-                    })
-                }
-            })
-            .catch(err => console.error(err));
-    }
-    //here handleOnchange takes search data for which we are searching for
-    const handleOnChange = (searchData) => {
-        setSearch(searchData);
-        onSearchChange(searchData);
-        
-    }
-    return (
-        //Here debounceTimeout={600}  a debounce function makes sure that our code is only triggered once per user input after 600ms the user get the data if he is typing fast for cities to get the weather data
-        // loadOptions is used for loading the options as if we search london then the user can only type the lond to get all the search options for london as user input the city which is async
-        <AsyncPaginate placeholder="Search for city" debounceTimeout={600} value={search} onChange={handleOnChange} loadOptions ={loadOptions } />
+  const [search, setSearch] = useState(null);
+  const loadOptions = (inputValue) => {
+    const geoAPIOptions = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "841d3d62e4msh4d331cc7eefed46p1ed864jsn450ba9ceb17c",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+      },
+    };
+
+    const geoAPIUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+    //here after return we cut the code from fetch to .catch function from api.js
+    //here ` ` is used for making the variable in js and ${} are variables inside that curly braces
+    return fetch(
+      `${geoAPIUrl}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoAPIOptions
     )
-}
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              //this is the format for searching the cities for avoiding the loadOptions error in consle
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
+  //here handleOnchange takes search data for which we are searching for
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+  return (
+    //Here debounceTimeout={600}  a debounce function makes sure that our code is only triggered once per user input after 600ms the user get the data if he is typing fast for cities to get the weather data
+    // loadOptions is used for loading the options as if we search london then the user can only type the lond to get all the search options for london as user input the city which is async
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
 
 export default Search;
